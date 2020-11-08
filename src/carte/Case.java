cdc71e6a7b5ea067ff7c8df6735a9dae1b407135
Louis Jézéquel-Royer <ixayceka3@gmail.com>
2020-11-08 19:17:58 +0100

    les dictionnaires

--- src/carte/Case.java
+++ src/carte/Case.java
@@ -7,10 +7,7 @@ public class Case {
 	private int colonne;
 	private Incendie incendie;
 	// Attributs pour le plus courts chemins :
-	private int f = 0;
-	private int g;
-	private int h;
-	private Case parent;
+
 
 	public Case(NatureTerrain natureTerrain, int lig, int col, Incendie incendie) {
 		super();
@@ -45,37 +42,7 @@ public class Case {
 		return "Case [nature=" + nature + ", ligne=" + ligne + ", colonne=" + colonne + ", incendie=" + incendie + "]";
 	}
 
-	public void setf() {
-		this.f = this.h + this.g;
-	}
 
-	public void setg(int g) {
-		this.g = g;
-	}
-
-	public void seth(int h) {
-		this.h = h;
-	}
-
-	public void setParent(Case parent) {
-		this.parent = parent;
-	}
-
-	public int getf() {
-		return this.f;
-	}
-
-	public int getg() {
-		return this.g;
-	}
-
-	public int geth() {
-		return this.h;
-	}
-
-	public Case getParent() {
-		return this.parent;
-	}
 
 	public Direction getDirection(Case destination){
 		if(destination.getLigne() - this.ligne == 1){
--- src/carte/Gps.java
+++ src/carte/Gps.java
@@ -1,6 +1,7 @@
 package carte;
 
 import java.util.ArrayList;
+import java.util.*;
 //import java.util.Math;
 
 import robot.Robot;
@@ -15,6 +16,11 @@ public class Gps {
 	private Robot robot;
 	private ArrayList<Case> fermees = new ArrayList<Case>();
 	private ArrayList<Case> ouverts = new ArrayList<Case>();
+
+	private Map<Case, Case> parents;
+	private Map<Case, Integer> valeursG;
+	private Map<Case, Integer> valeursH;
+
 	private Case debut;
 	private Case fin;
 
@@ -24,12 +30,42 @@ public class Gps {
 			this.debut = debut;
 			this.fin = fin;
 		}
+		this.parents = new HashMap<Case, Case>();
+		this.valeursG = new HashMap<Case, Integer>();
+		this.valeursH = new HashMap<Case, Integer>();
 	}
+
+	public void setParent(Case fille, Case parent){
+			this.parents.put(fille, parent);
+	}
+
+	public Case getParent(Case fille){
+		return this.parents.get(fille);
+	}
+
+	public void setG(Case caseActuelle, int valeur){
+		this.valeursG.put(caseActuelle, valeur);
+	}
+
+	public int getG(Case caseActuelle){
+		return this.valeursG.get(caseActuelle);
+	}
+
+	public void setH(Case caseCible){
+		this.valeursH.put(caseCible, this.calculManhattan(caseCible, this.fin));
+	}
+
+	public int getH(Case caseCible){
+		return this.valeursH.get(caseCible);
+	}
+
 
 
 	// On pourrait renvoyer un ArrayList qui contient le chemin trouvé ou null si pas de chemin
 	public void trouverChemin(Simulateur simul, DonneesSimulation donnees) {
 		this.ouverts.add(this.debut);
+		this.setG(this.debut, 0);
+		this.setH(this.debut);
 		// Créer une variable g qui augmente à chaque fois qu'on rajoute une étape au chemin
 
 		while(!ouverts.isEmpty()){
@@ -44,27 +80,28 @@ public class Gps {
 			this.ouverts.remove(current);
 			for(Case caseVoisine : voisins){
 
-				caseVoisine.seth(calculManhattan(caseVoisine, this.fin));
+
 				if(!this.peutMarcher(caseVoisine) || this.fermees.contains(caseVoisine)){
 					;
 				}
 				else{
 					if(!this.ouverts.contains(caseVoisine)){
-						caseVoisine.setParent(current);
-						caseVoisine.seth(calculManhattan(caseVoisine, this.fin));
-						caseVoisine.setg(current.getg() + this.robot.tempsAccesVoisin(current.getDirection(caseVoisine)));
+						this.setH(caseVoisine);
+						this.setParent(caseVoisine, current);
+						this.setG(caseVoisine, this.getG(current) + this.robot.tempsAccesVoisin(current, current.getDirection(caseVoisine)));
 						this.ouverts.add(caseVoisine);
 					}
 					else{
-						if(caseVoisine.getg() > current.getg() +  this.robot.tempsAccesVoisin(current.getDirection(caseVoisine))){
-							caseVoisine.setParent(current);
-							caseVoisine.setg(current.getg() +  this.robot.tempsAccesVoisin(current.getDirection(caseVoisine)));
+						if(this.getG(caseVoisine) > this.getG(current) +  this.robot.tempsAccesVoisin(current, current.getDirection(caseVoisine))){
+							this.setParent(caseVoisine,current);
+							this.setG(caseVoisine, this.getG(current) +  this.robot.tempsAccesVoisin(current, current.getDirection(caseVoisine)));
 						}
 					}
 				}
 			}
 
 		}
+		System.out.println("Chemin non trouvé");
 	}
 
 	private int calculManhattan(Case case1, Case case2){
@@ -77,28 +114,28 @@ public class Gps {
 
 	private Case trouverFMini(ArrayList<Case> ouverts){
 		int indiceMin = 0;
-		int valeurMin = ouverts.get(0).getg() + ouverts.get(0).geth();
+		int valeurMin = this.getG(ouverts.get(0)) + this.getH(ouverts.get(0));
 		for(int curseur = 1; curseur < ouverts.size(); curseur++){
 			// Au lieu de faire un get, on peut calculer f, en rajoutant g en paramètre
 			// Genre f = g + calcul_manhattan(fin, case)
 			// Pour l'initialisation de valeur min, on peut mettre le plus grand int existant
-			if(ouverts.get(curseur).getg() + ouverts.get(curseur).geth() < valeurMin){
+			if(this.getG(ouverts.get(curseur)) + this.getH(ouverts.get(curseur)) < valeurMin){
 				indiceMin = curseur;
-				valeurMin = ouverts.get(curseur).getg() + ouverts.get(curseur).geth();
+				valeurMin = this.getG(ouverts.get(curseur)) + this.getH(ouverts.get(curseur));
 			}
 		}
 		return ouverts.get(indiceMin);
 	}
 
 	private long creationEvenementChemin(Case caseChemin, Simulateur simul, DonneesSimulation donnees){
-			if(caseChemin.getParent() == null){
+			if(!this.parents.containsKey(caseChemin)){
 				return simul.getDateSimulation();
 			}
 			else{
-				long ancienneDate = creationEvenementChemin(caseChemin.getParent(), simul, donnees);
-				DeplacementDebut nouvelleEtapeChemin = new DeplacementDebut(ancienneDate, simul, this.robot, caseChemin.getParent().getDirection(caseChemin), donnees.getCarte());
+				long ancienneDate = creationEvenementChemin(this.getParent(caseChemin), simul, donnees);
+				DeplacementDebut nouvelleEtapeChemin = new DeplacementDebut(ancienneDate, simul, this.robot, this.getParent(caseChemin).getDirection(caseChemin), donnees.getCarte());
 				simul.ajouteEvenement(nouvelleEtapeChemin);
-				ancienneDate += this.robot.tempsAccesVoisin(caseChemin.getParent().getDirection(caseChemin));
+				ancienneDate += this.robot.tempsAccesVoisin(this.getParent(caseChemin).getDirection(caseChemin));
 				return ancienneDate;
 			}
 	}
