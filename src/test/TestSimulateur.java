54c2f14ef819a49eb1d1e4c4d44bfcf25d08b9bc
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2020-11-18 23:19:04 +0100

    Changement des images après incendies

--- src/test/TestSimulateur.java
+++ src/test/TestSimulateur.java
@@ -13,10 +13,10 @@ public class TestSimulateur {
 		// crée la fenêtre graphique dans laquelle dessiner
 		GUISimulator gui = new GUISimulator(800, 600, Color.BLACK);
 
-        String cartePath = "cartes/mushroomOfHell-20x20.map";
+//        String cartePath = "cartes/mushroomOfHell-20x20.map";
 //        String cartePath = "cartes/spiralOfMadness-50x50.map";
 //        String cartePath = "cartes/desertOfDeath-20x20.map";
-//        String cartePath = "cartes/carteSujet.map";
+        String cartePath = "cartes/carteSujet.map";
         
 		Simulateur simul = new Simulateur(gui, cartePath);
 	}
