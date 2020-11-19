dd8fedbb77572f1747c3cb15bf44c7bea35a9b70
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-11-19 15:16:03 +0100

    Commentaire en trop

--- src/strategie/ChefPompier.java
+++ src/strategie/ChefPompier.java
@@ -105,7 +105,6 @@ public class ChefPompier {
 				} else {
 					Gps chemin = new Gps(robots[j], robots[j].getPosition(), fire.getPosition());
 					if (chemin.trouverChemin(simul, donnees)) {
-//						System.out.println(j + ": Je suis la");
 						long dateArrivee = chemin.dateArrivee(simul);
 						if (dateArrivee < tempsMin) {
 							robMin = robots[j];
